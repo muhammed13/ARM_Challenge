df4e7be265ad4e5d7ca70f3cbee9aa732047a03f
muhammed13 <40000647+muhammed13@users.noreply.github.com>
2020-04-10 00:10:07 +0200

    PortDriver Added

--- NVIC.c
+++ NVIC.c
@@ -8,52 +8,52 @@ extern NVIC_ConfigType NVIC_INTsConfiguration;
 
 void NVIC_init(void)
 {
-			uint8_t temp_interrupt_number;
-			uint8_t temp_group_priority;
-			uint8_t temp_subgroup_priority;
-			uint8_t INT_Enable_Register_Number;
-			uint8_t INT_Enable_Bit_Number;
-			uint8_t Priority_Register_Number;
-			uint8_t Priority_Filed_Number;
-			uint32_t INT_register_address;
-			uint32_t Priority_register_address;
-			uint8_t i;
-			
-			/*SET THE COMPINATION APINT*(xxx,xxy,xyy...*/
-			NVIC_APINT |= ( 0xffff0000 | (0x05FA<<16));    /*this  field must be written to this value in order to change the bits in this register*/
-			NVIC_APINT |= ( 0x00000700 | (0x6<<8));  /*set the priority compination to be xyy (goup subgroup subgroup)*/ 
-			
-			for (i=0;i<NUMBER_OF_ACTIVATED_INTERRUPTS;i++)
-			{
-		    		/* save each interrupt information in a temporary variables */ 
-						temp_interrupt_number=NVIC_INTsConfiguration.Pool[i].interrupt_number;  
-						temp_group_priority=NVIC_INTsConfiguration.Pool[i].group_priority;
-						temp_subgroup_priority=NVIC_INTsConfiguration.Pool[i].subgroup_priority;
-						
-						/*ENABLE INTERRUPT USING EN0,EN1...*/
-						INT_Enable_Register_Number=temp_interrupt_number/32;   /*divide by 32 since each register have 32 interrupt enable bit for 32 interrupt*/
-						if(temp_interrupt_number<32)
-						{
-							  /*if the interrupt locates at the first register*/
-								INT_Enable_Bit_Number=temp_interrupt_number;          
-						}
-						else
-						{
-							   /*if the interrupt dos'nt locate at the first register */
-								INT_Enable_Bit_Number=temp_interrupt_number%32;      
-						}
-						/* to find the register address => I added the  address of ENT0  which is the start address here to enable register number multiplied by 4 
-						*  since the registers size is 4bytes. I added one because the First register number is zero*/
-						INT_register_address=NVIC_EN0+((INT_Enable_Register_Number+1)*4);       
-						(*((volatile uint32_t *)INT_register_address))  |=  (1<<INT_Enable_Bit_Number);    /*set the interrupt enable bit by 1*/
-						
-						/*SET PEIORITY(group and subgroup)USING PR0 ,PR1 ...*/
-						Priority_Register_Number=temp_interrupt_number/4;  /*divide by 4 since each register has four interrupts*/
-						Priority_Filed_Number=temp_interrupt_number%4;      /* determine whhich field (0=>A , 1=>B , 3=>C, 4=>D)*/
-						Priority_register_address=NVIC_PRI0+((INT_Enable_Register_Number+1)*4);       /* same as how I get the INT_register_address*/
-						/*set the priority register by the subgroup priority value shifted by the priority field number mulltiplied by 8 difference between the start of a field and the start of 
-						* the next field is 8 bits. and I added 5 since the first field in the register started at bit 5*/
-						(*((volatile uint32_t *)Priority_register_address))  |=  (temp_subgroup_priority<<((Priority_Filed_Number*8)+5));      
-						(*((volatile uint32_t *)Priority_register_address))  |=  (temp_group_priority<<((Priority_Filed_Number*8)+7));  /*I added 7 here instead of 5 since first group priority bit is  bit 7*/               
-				}
+	uint8_t temp_interrupt_number;
+	uint8_t temp_group_priority;
+	uint8_t temp_subgroup_priority;
+	uint8_t INT_Enable_Register_Number;
+	uint8_t INT_Enable_Bit_Number;
+	uint8_t Priority_Register_Number;
+	uint8_t Priority_Filed_Number;
+	uint32_t INT_register_address;
+	uint32_t Priority_register_address;
+	uint8_t i;
+
+	/*SET THE COMPINATION APINT*(xxx,xxy,xyy...*/
+	NVIC_APINT |= ( 0xffff0000 | (0x05FA<<16)) | ( 0x00000700 | (0x6<<8)); /*this  field must be written to this value in order to change the bits in this register*/
+														                   /*set the priority compination to be xyy (goup subgroup subgroup)*/
+
+	for (i=0;i<NUMBER_OF_ACTIVATED_INTERRUPTS;i++)
+	{
+	   /* save each interrupt information in a temporary variables */
+		temp_interrupt_number=NVIC_INTsConfiguration.Pool[i].interrupt_number;
+		temp_group_priority=NVIC_INTsConfiguration.Pool[i].group_priority;
+		temp_subgroup_priority=NVIC_INTsConfiguration.Pool[i].subgroup_priority;
+
+		/*ENABLE INTERRUPT USING EN0,EN1...*/
+		INT_Enable_Register_Number=temp_interrupt_number/32;   /*divide by 32 since each register have 32 interrupt enable bit for 32 interrupt*/
+		if(temp_interrupt_number<32)
+		{
+			  /*if the interrupt locates at the first register*/
+				INT_Enable_Bit_Number=temp_interrupt_number;
+		}
+		else
+		{
+			   /*if the interrupt dos'nt locate at the first register */
+				INT_Enable_Bit_Number=temp_interrupt_number%32;
+		}
+		/* to find the register address => I added the  address of ENT0  which is the start address here to enable register number multiplied by 4
+		*  since the registers size is 4bytes. I added one because the First register number is zero*/
+		INT_register_address=NVIC_EN0+((INT_Enable_Register_Number+1)*4);
+		(*((volatile uint32_t *)INT_register_address))  |=  (1<<INT_Enable_Bit_Number);    /*set the interrupt enable bit by 1*/
+
+		/*SET PEIORITY(group and subgroup)USING PR0 ,PR1 ...*/
+		Priority_Register_Number=temp_interrupt_number/4;  /*divide by 4 since each register has four interrupts*/
+		Priority_Filed_Number=temp_interrupt_number%4;      /* determine whhich field (0=>A , 1=>B , 3=>C, 4=>D)*/
+		Priority_register_address=NVIC_PRI0+((INT_Enable_Register_Number+1)*4);       /* same as how I get the INT_register_address*/
+		/*set the priority register by the subgroup priority value shifted by the priority field number mulltiplied by 8 difference between the start of a field and the start of
+		* the next field is 8 bits. and I added 5 since the first field in the register started at bit 5*/
+		(*((volatile uint32_t *)Priority_register_address))  |=  (temp_subgroup_priority<<((Priority_Filed_Number*8)+5));
+		(*((volatile uint32_t *)Priority_register_address))  |=  (temp_group_priority<<((Priority_Filed_Number*8)+7));  /*I added 7 here instead of 5 since first group priority bit is  bit 7*/
+	}
 }
--- Register.h
+++ Register.h
@@ -1,21 +1,45 @@
+/*
+ ============================================================================
+ Name        : GPIO_Cfg.c
+ Author      : Muhammed Gamal
+ Description :
+ ============================================================================
+ */
+
+/*********************************includes**********************************/
+#include "std_types.h"
+/***************************************************************************/
+
 
 /***********************************NVIC*************************************************/
-#define NVIC_APINT                        (*((volatile unsigned long*)0xE000ED0C))
-#define NVIC_EN0                           (*((volatile unsigned long *)0xE000E100))
-#define NVIC_PRI0                          (*((volatile unsigned long *)0xE000E400))  
-	
-/****************************************************************************************/
-
-
+#define NVIC_APINT                         (*((volatile uint32_t*)0xE000ED0C))
+#define NVIC_EN0                           (*((volatile uint32_t*)0xE000E100))
+#define NVIC_PRI0                          (*((volatile uint32_t*)0xE000E400))
 
 /*******************************System Control***************************************/
-#define RCC2                       (*((volatile unsigned long*)0x400FE070))
-	            /***********************RCC2 BIits************************/
+#define RCC2                               (*((volatile uint32_t*)0x400FE070))
+/*******RCC2 BIits********/
 #define USERCC2                            31
-#define OSCSRC2_1                         4
-#define OSCSRC2_2                         5
-#define OSCSRC2_3                         3
-#define DIV400                                 30
-#define SYSDIV2LSB                        22
-	
+#define OSCSRC2_1                          4
+#define OSCSRC2_2                          5
+#define OSCSRC2_3                          3
+#define DIV400                             30
+#define SYSDIV2LSB                         22
+
+
+/****************************************GPIO******************************************/
+#define RCGCGPIO                           (*((volatile uint32_t*)0x400FE608))
+#define PORTA_GPIODIR                      (*((volatile uint32_t*)0x40004400))
+#define PORTB_GPIODIR                      (*((volatile uint32_t*)0x40005400))
+#define PORTC_GPIODIR                      (*((volatile uint32_t*)0x40006400))
+#define PORTD_GPIODIR                      (*((volatile uint32_t*)0x40007400))
+#define PORTE_GPIODIR                      (*((volatile uint32_t*)0x40024400))
+#define PORTF_GPIODIR                      (*((volatile uint32_t*)0x40025400))
+
+#define PORTA_GPIOAFSEL                    (*((volatile uint32_t*)0x40004420))
+#define PORTB_GPIOAFSEL                    (*((volatile uint32_t*)0x40005420))
+#define PORTC_GPIOAFSEL                    (*((volatile uint32_t*)0x40006420))
+#define PORTD_GPIOAFSEL                    (*((volatile uint32_t*)0x40007420))
+#define PORTE_GPIOAFSEL                    (*((volatile uint32_t*)0x40024420))
+#define PORTF_GPIOAFSEL                    (*((volatile uint32_t*)0x40025420))
 /****************************************************************************************/
